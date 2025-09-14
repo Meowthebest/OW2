5de916577f29b7cc572161b34f79d0af36f180fc
Meowthebest <138353441+Meowthebest@users.noreply.github.com>
2025-09-14 16:03:10 +0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Overwatch2RandomHeroPickerDuoRoleLock from '@/components/Overwatch2RandomHeroPickerDuoRoleLock'
+
+export default function App() {
+  return (
+    <div className="min-h-screen p-4 md:p-8">
+      <Overwatch2RandomHeroPickerDuoRoleLock />
+    </div>
+  )
+}
