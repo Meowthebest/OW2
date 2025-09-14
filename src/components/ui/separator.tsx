614f839d58efa3ad159ce54d4675f7d382116e2d
Meowthebest <138353441+Meowthebest@users.noreply.github.com>
2025-09-14 16:47:15 +0400

    Update separator.tsx

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,7 +1,6 @@
-* as React from 'react'
+import * as React from 'react'
 import { cn } from '@/lib/cn'
 
-
 export function Separator({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-return <div className={cn('h-px w-full bg-zinc-200 dark:bg-zinc-800', className)} {...props} />
+  return <div className={cn('h-px w-full bg-zinc-200 dark:bg-zinc-800', className)} {...props} />
 }
